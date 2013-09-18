10ccf1f8f2f83b7ff3f6fcc98c5793596c9cf1f0
Sibi <sibi@psibi.in>
2013-09-18 14:59:22 +0530

    All the core functionalities implemented.

--- src/gol.hs
+++ src/gol.hs
@@ -13,7 +13,6 @@ instance Show Cell where
   show Alive = "Alive Cell"
   show Dead = "Dead Cell"
 
-
 -- Returns the max coordinates of a world
 max_coordinate :: World -> Location
 max_coordinate xs  = aux xs (0, 0)
@@ -57,20 +56,20 @@ alive_cells world = map (\x -> fst x) alive
   where alive = filter (\x -> cell_state world (fst x) == Alive) world
 
 -- Apply Conway GOL Rule for a Cell
-game_of_life :: World -> Location -> Cell
+game_of_life :: World -> Location -> (Location,Cell)
 game_of_life world loc = case (cell_state world loc) of Dead -> if num_alive_neighbours world loc == 3
-                                                                then Alive
-                                                                else Dead
+                                                                then (loc, Alive)
+                                                                else (loc, Dead)
                                                         Alive -> num_alive_neighbours_cells world loc
   where num_alive_neighbours_cells world loc
-          | num_alive_neighbours world loc > 3 = Dead
-          | num_alive_neighbours world loc < 2 = Dead
-          | num_alive_neighbours world loc == 2 || num_alive_neighbours world loc == 3 = Alive
+          | num_alive_neighbours world loc > 3 = (loc, Dead)
+          | num_alive_neighbours world loc < 2 = (loc, Dead)
+          | num_alive_neighbours world loc == 2 || num_alive_neighbours world loc == 3 = (loc, Alive)
         
 -- One time step for the game of life
--- time_step :: World -> World
--- time_step world = 
+time_step :: World -> World
+time_step world = map (\x -> game_of_life world (fst x)) world
 
 --let world_data = [((0,0), Alive), ((0,1), Dead), ((0,2), Alive), ((0,3), Dead),((1,0), Alive), ((1,1), Dead), ((1,2), Alive), ((1,3), Dead),((2,0), Alive), ((2,1), Alive), ((2,2), Dead), ((2,3), Alive),((3,0), Alive), ((3,1), Dead), ((3,2), Alive), ((3,3), Alive)] :: World
 
-another_world_data = [((0,0), Dead), ((0,1), Alive), ((0,2), Dead), ((1,0), Dead), ((1,1), Alive), ((1,2), Dead), ((2,0), Dead), ((2,1), Alive), ((2,2), Dead)]
+--let another_world_data = [((0,0), Dead), ((0,1), Alive), ((0,2), Dead), ((1,0), Dead), ((1,1), Alive), ((1,2), Dead), ((2,0), Dead), ((2,1), Alive), ((2,2), Dead)] :: World
