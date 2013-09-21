809db7670ea04895c9caecbc9333bb5bef27c4fe
Sibi <sibi@psibi.in>
2013-09-21 08:34:00 +0530

    Indentation made.

--- src/Gol.hs
+++ src/Gol.hs
@@ -23,12 +23,12 @@ cell_state world loc = case world of [] -> Dead
 -- Returns all the neighbours for a cell
 neighbours :: Location -> [Location]
 neighbours loc = case loc of (a,b) -> filter (\x -> x /= loc) all_neighbours
-                                      where all_neighbours = [(x,y) | x <- [a-1..a+1], y <- [b-1..b+1],  x >= 0, y >= 0]
+                               where all_neighbours = [(x,y) | x <- [a-1..a+1], y <- [b-1..b+1],  x >= 0, y >= 0]
 
 -- Returns the number of alive neighbours for a cell
 num_alive_neighbours :: World -> Location -> Int
 num_alive_neighbours world loc = foldl (\acc x -> if x==Alive then acc+1 else acc) 0 neighbour_states
-    where neighbour_states = map (\x -> cell_state world x) (neighbours loc)
+  where neighbour_states = map (\x -> cell_state world x) (neighbours loc)
 
 -- Returns the location of all alive neighbours
 alive_neighbours :: World -> Location -> [Location]
